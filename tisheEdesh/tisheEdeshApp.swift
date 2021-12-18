6bf6be536177b93cbce030e6ec1aafe43c37ce7b
sss <kirillku4mar24@gmail.com>
2021-12-18 16:50:25 +0300

    Initial Commit

---
+++ tisheEdesh/tisheEdeshApp.swift
@@ -0,0 +1,17 @@
+//
+//  tisheEdeshApp.swift
+//  tisheEdesh
+//
+//  Created by Кирилл Кучмар on 18.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct tisheEdeshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
